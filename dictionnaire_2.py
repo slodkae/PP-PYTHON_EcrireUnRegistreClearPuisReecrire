2e902d337812bdc0f75a4bf65e64e240b791d433
slodkae <elena.zapolny@epitech.eu>
2022-01-04 17:53:03 +0100

    final project

---
+++ dictionnaire_2.py
@@ -0,0 +1,8 @@
+taux_de_conversion = {}
+taux_de_conversion['facebook'] = 3.4
+taux_de_conversion['instagram'] = 1.2
+taux_de_conversion = dict()
+taux_de_conversion['facebook'] = 3.4
+taux_de_conversion['instagram'] = 1.2
+
+print(taux_de_conversion)
